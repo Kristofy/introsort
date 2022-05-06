046ecffa0a76af9f7ab15b4371857663232f787e
Osztopáni Kristóf <kristofosztopani@gmail.com>
2022-05-06 15:59:43 +0200

    save

--- src/js/bootstrap.bundle.js
+++ src/js/bootstrap.bundle.js
@@ -5282,11 +5282,11 @@
     div: [],
     em: [],
     hr: [],
+    h6: [],
     h1: [],
     h2: [],
     h3: [],
     h4: [],
-    h5: [],
     h6: [],
     i: [],
     img: ['src', 'srcset', 'alt', 'title', 'width', 'height'],
@@ -6137,7 +6137,7 @@
     offset: [0, 8],
     trigger: 'click',
     content: '',
-    template: '<div class="popover" role="tooltip">' + '<div class="popover-arrow"></div>' + '<h3 class="popover-header"></h3>' + '<div class="popover-body"></div>' + '</div>'
+    template: '<div class="popover" role="tooltip">' + '<div class="popover-arrow"></div>' + '<h2 class="popover-header"></h2>' + '<div class="popover-body"></div>' + '</div>'
   };
   const DefaultType$2 = { ...Tooltip.DefaultType,
     content: '(null|string|element|function)'
